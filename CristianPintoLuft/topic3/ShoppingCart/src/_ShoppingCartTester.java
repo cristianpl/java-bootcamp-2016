1cb9ed760ad69bfa97b9712f896486c3c04cc4d8
Cristian <Cristian>
2016-03-06 12:10:46 -0300

    updated test to show total prices when the user adds and deletes items in the shopping cart

--- CristianPintoLuft/topic3/ShoppingCart/src/_ShoppingCartTester.java
+++ CristianPintoLuft/topic3/ShoppingCart/src/_ShoppingCartTester.java
@@ -68,4 +68,25 @@ public class _ShoppingCartTester {
 		cart.addItem(bread, 1);
 		assertEquals(true, cart.updateItem(cheese, "Cream cheese"));
 	}
+
+	@Test
+	public void buyItems() {
+		ShoppingCart cart = new ShoppingCart();
+		Item cheese = new Item("Cheese", 98.06);
+		Item bread = new Item("Bread", 10.15);
+		Item meal = new Item("Meal", 15.20);
+		Item beef = new Item("Beef", 35.50);
+		cart.addItem(cheese, 2);
+		cart.addItem(bread, 1);
+		cart.addItem(meal, 2);
+		cart.addItem(beef, 3);
+		/*
+		 * In here, the carts total price should be $343.17, like in the
+		 * calculatePrice test. If I delete some items from it, the price should
+		 * be lower than this.
+		 */
+		cart.deleteItem(beef, 2);
+		cart.deleteItem(cheese, 1);
+		assertEquals(174.11, cart.totalPrice(), 0.01);
+	}
 }
